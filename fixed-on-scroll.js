908fe0a0a4c806df87e45b0872d027c77df8c860
Simon Destombes <simon@vercors-net.com>
2016-12-16 22:45:49 +0100

    Initial commit

---
+++ fixed-on-scroll.js
@@ -0,0 +1,29 @@
+/**
+ * Menu fixe quand il arrive en haut de page
+
+<div class="nav-wrapper">
+	<nav class="navbar-fixed-top fixed-on-scroll">Ici le menu</nav>
+</div>
+*/
+
+require([ 'jquery' ], function($) {
+	$(function() {
+		var el = $('.fixed-on-scroll');
+		var origOffsetY = el.offset().top;
+
+		function scroll() {
+			if ($(window).scrollTop() >= origOffsetY) {
+				el.css({
+					'position' : 'fixed'
+				});
+			} else {
+				el.css({
+					'position' : 'relative'
+				});
+			}
+		}
+
+		window.onscroll = scroll;
+	});
+
+});
